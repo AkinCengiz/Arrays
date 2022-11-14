6a7149ccabee06736de38765ad4093440c68dad0
Akın CENGİZ <akincengiz@outlook.com.tr>
2022-11-14 16:32:39 +0300

    Dosyalar yüklendi.

---
+++ MultiDimensionalArray/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiDimensionalArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int deger = 15;
+            int[,] dizi = new int[15, 15];
+            for (int i = 0; i < 15; i++)
+            {
+                for (int j = 0; j < 15; j++)
+                {
+                    if (j == i)
+                    {
+                        dizi[i, j] = 1;
+                        dizi[i, deger - j - 1] = 1;
+                    }
+
+                    if (i == deger / 2)
+                    {
+                        dizi[i, j] = 1;
+                    }
+
+                    if (j == deger / 2)
+                    {
+                        dizi[i, j] = 1;
+                    }
+                }
+            }
+
+            for (int i = 0; i < deger; i++)
+            {
+                for (int j = 0; j < deger; j++)
+                {
+                    if (dizi[i, j] == 1)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RandomArrays/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] rastgeleDizi = new int[100];
+            for (int i = 0; i < rastgeleDizi.Length; i++)
+            {
+                rastgeleDizi[i]=random.Next(1,11);
+            }
+            int[] adet = new int[10];
+            for (int i = 0; i < 100; i++)
+            {
+                adet[rastgeleDizi[i] - 1]++;
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("{0,2} Adet -----> {1,2} ", adet[i], i + 1);
+                for (int j = 0; j < adet[i]; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
